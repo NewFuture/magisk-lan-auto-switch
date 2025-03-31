19913a83f03b45d5df467e48e3e7081d2eaf4a4b
New Future <NewFuture@users.noreply.github.com>
2025-03-31 17:43:09 +0000

    fix release permission

--- service.sh
+++ service.sh
@@ -1,15 +1,21 @@
 #!/system/bin/sh
 
-# Delay execution by 3 seconds
-sleep 3
+ui_print "[LAS] lan-auto-switch script started:"
+
+# Delay execution
+sleep 1
 
 # Check if eth0 is connected
 ETH0_CONNECTED=$(cat /sys/class/net/eth0/carrier 2>/dev/null)
 
 if [ "$ETH0_CONNECTED" = "1" ]; then
     # eth0 is connected, turn off WiFi
+    ui_print "[LAS] eth0 is connected. Disabling WiFi."
     svc wifi disable
 else
     # eth0 is not connected, turn on WiFi
+    ui_print "[LAS] eth0 is not connected. Enabling WiFi."
     svc wifi enable
 fi
+
+ui_print "[LAS] Script finished!"
