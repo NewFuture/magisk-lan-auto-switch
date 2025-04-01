1fc0eb593d5de508159ca9e50f511bc13f0f275c
New Future <NewFuture@users.noreply.github.com>
2025-04-01 03:54:27 +0000

    ensure boot_completed

--- service.sh
+++ service.sh
@@ -2,11 +2,17 @@
 
 log -t Magisk "[lan-auto-swith] script started:"
 
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+    sleep 1
+done
+
 # Delay execution
-sleep 10
+sleep 15
 
 # Check if eth0 is connected
 ETH0_CONNECTED=$(cat /sys/class/net/eth0/carrier 2>/dev/null)
+
+log -t Magisk "[lan-auto-swith] eth0/carrier=${ETH0_CONNECTED}"
 
 if [ "$ETH0_CONNECTED" = "1" ]; then
     log -t Magisk "[lan-auto-swith] eth0 is connected. Disabling WiFi."
@@ -16,4 +22,4 @@ else
     svc wifi enable
 fi
 
-log -t Magisk "[lan-auto-swith] Script finished!"
+log -t Magisk "[lan-auto-swith] script finished!"
