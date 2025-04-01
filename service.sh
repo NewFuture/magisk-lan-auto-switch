5cf808da76d641619d40042bfe20d9f9459fa9d0
New Future <NewFuture@users.noreply.github.com>
2025-04-01 04:27:29 +0000

    add more cmds

--- service.sh
+++ service.sh
@@ -3,11 +3,11 @@
 log -t Magisk "[lan-auto-swith] script started:"
 
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
-    sleep 1
+    sleep 5
 done
 
 # Delay execution
-sleep 15
+sleep 25
 
 # Check if eth0 is connected
 ETH0_CONNECTED=$(cat /sys/class/net/eth0/carrier 2>/dev/null)
@@ -17,9 +17,15 @@ log -t Magisk "[lan-auto-swith] eth0/carrier=${ETH0_CONNECTED}"
 if [ "$ETH0_CONNECTED" = "1" ]; then
     log -t Magisk "[lan-auto-swith] eth0 is connected. Disabling WiFi."
     svc wifi disable
+    settings put global wifi_on 0
+    settings put global wifi_scan_always_enabled 0
+    cmd -w wifi set-wifi-enabled disabled 
 else
     log -t Magisk "[lan-auto-swith] eth0 is not connected. Enabling WiFi."
     svc wifi enable
+    settings put global wifi_on 1
+    settings put global wifi_scan_always_enabled 1
+    cmd -w wifi set-wifi-enabled enabled 
 fi
 
 log -t Magisk "[lan-auto-swith] script finished!"
