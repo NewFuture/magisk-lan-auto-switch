565ff98f90a08b7838a074fae308ee7cff3f2c03
New Future <NewFuture@users.noreply.github.com>
2025-04-01 02:43:02 +0000

    delay l0s

--- service.sh
+++ service.sh
@@ -1,19 +1,19 @@
 #!/system/bin/sh
 
-log -t Magisk_LAS "[lan-auto-swith] script started:"
+log -t Magisk "[lan-auto-swith] script started:"
 
 # Delay execution
-sleep 1
+sleep 10
 
 # Check if eth0 is connected
 ETH0_CONNECTED=$(cat /sys/class/net/eth0/carrier 2>/dev/null)
 
 if [ "$ETH0_CONNECTED" = "1" ]; then
-    log -t Magisk_LAS "[lan-auto-swith] eth0 is connected. Disabling WiFi."
+    log -t Magisk "[lan-auto-swith] eth0 is connected. Disabling WiFi."
     svc wifi disable
 else
-    log -t Magisk_LAS "[lan-auto-swith] eth0 is not connected. Enabling WiFi."
+    log -t Magisk "[lan-auto-swith] eth0 is not connected. Enabling WiFi."
     svc wifi enable
 fi
 
-log -t Magisk_LAS "[lan-auto-swith] Script finished!"
+log -t Magisk "[lan-auto-swith] Script finished!"
