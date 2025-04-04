5da36d8daa15a598bd88ff12ce7e8dcb039b8120
New Future <NewFuture@users.noreply.github.com>
2025-04-04 03:55:32 +0000

    add fake battery

---
+++ magisk-fake-battery/README.md
@@ -0,0 +1,5 @@
+# magisk-fake-battery
+
+fake for device removed batttery
+
+download from release and install
---
+++ magisk-fake-battery/service.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+# wait boot complete and battery service ready
+
+log -t Magisk "[fake-battery] Start"
+
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+    sleep 1
+done
+
+sleep 1
+
+dumpsys battery set level 100
+dumpsys battery set status 5
+dumpsys battery set temp 25
+dumpsys battery set ac 1
+dumpsys battery set present 0
+
+log -t Magisk "[fake-battery] Fake battery done"
