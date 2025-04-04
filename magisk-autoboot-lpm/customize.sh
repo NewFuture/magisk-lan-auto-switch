8e083f06dc9d183d0e0f7a18677112798895369c
New Future <NewFuture@users.noreply.github.com>
2025-04-04 21:58:43 +0800

    Update customize.sh

--- magisk-autoboot-lpm/customize.sh
+++ magisk-autoboot-lpm/customize.sh
@@ -1,6 +1,7 @@
 
 ui_print "- Install autoboot to boot.img"
 
+set_perm_recursive $MODPATH/files 0 2000 0644 0755
 . $MODPATH/patch.sh "add 0700 overlay.d/init.autoboot.rc files/init.autoboot.rc" 
 
 ui_print "Success !"
