3771b31384f007238b39768ae6e8e2c74ac7a2cd
New Future <NewFuture@users.noreply.github.com>
2025-04-06 21:44:38 +0800

    Update action.sh

--- magisk-fake-battery/action.sh
+++ magisk-fake-battery/action.sh
@@ -1,15 +1,16 @@
 #!/system/bin/sh
 
-log -t Magisk "[fake-battery] check battery level"
+ui_print "[fake-battery] check battery level"
 
 # Get the current fake battery level
 current_level=$(dumpsys battery | grep "level:" | awk '{print $2}')
 
 if [ "$current_level" -eq 100 ]; then
-    log -t Magisk "[fake-battery] Resetting battery status..."
+    ui_print "[fake-battery] Resetting battery status..."
     dumpsys battery reset
-    log -t Magisk "[fake-battery] Battery status reset."
+    ui_print "[fake-battery] Battery status reset."
 else
-    log -t Magisk "[fake-battery] $current_level. Reapplying fake battery settings..."
+    ui_print "[fake-battery] $current_level. Reapplying fake battery settings..."
     . $MODPATH/service.sh
+    ui_print "set done"
 fi
