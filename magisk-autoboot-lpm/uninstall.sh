0415e8693e5a7e71ba4594520e614589e6a72a92
New Future <NewFuture@users.noreply.github.com>
2025-04-06 20:53:21 +0800

    Update uninstall.sh

--- magisk-autoboot-lpm/uninstall.sh
+++ magisk-autoboot-lpm/uninstall.sh
@@ -1,8 +1,14 @@
 
-ui_print "- Uninstall autoboot"
+#!/system/bin/sh
 
-. $MODPATH/patch.sh "rm overlay.d/init.autoboot.rc" 
+MODPATH=${0%/*}
 
-ui_print "init.autoboot.rc removed!"
+ORIGINAL_BOOT=$MODPATH/backup_boot.img
+get_flags
+find_boot_image
+flash_image $ORIGINAL_BOOT "$BOOTIMAGE"
 
-ui_print "Success !"
+# ui_print "- Uninstall autoboot"
+# . $MODPATH/patch.sh "rm overlay.d/init.autoboot.rc" 
+# ui_print "init.autoboot.rc removed!"
+# ui_print "Success !"
