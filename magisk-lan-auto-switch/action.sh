f692abe8468c80820b32fd8f50d80173df26f8e8
New Future <NewFuture@users.noreply.github.com>
2025-04-08 02:08:17 +0000

    update lan switch

--- magisk-lan-auto-switch/action.sh
+++ magisk-lan-auto-switch/action.sh
@@ -15,10 +15,12 @@ L "eth0/carrier=${ETH0_CONNECTED}"
 if [ "$ETH0_CONNECTED" = "1" ]; then
     L "eth0 is connected. Disabling WiFi."
     svc wifi disable
+    settings put global wifi_on 0
     cmd -w wifi set-wifi-enabled disabled 
 else
     L "eth0 is not connected. Enabling WiFi."
     svc wifi enable
+    settings put global wifi_on 1
     cmd -w wifi set-wifi-enabled enabled 
 fi
 
